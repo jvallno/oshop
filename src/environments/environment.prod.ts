4fc5a4c0a2497d7897ecbccea2f267b2adf31e48
jvallno <jurjani.valles@gmail.com>
2021-11-15 19:24:52 +0800

    First commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyAt2rLv9wSCb7-1K3b_P_LzumvNM2pCH7A",
+    authDomain: "oshop-678a5.firebaseapp.com",
+    projectId: "oshop-678a5",
+    storageBucket: "oshop-678a5.appspot.com",
+    messagingSenderId: "162127482175",
+    appId: "1:162127482175:web:c2477046f737cdf54bd78a",
+    measurementId: "G-SRMXNF1T4R" 
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyAt2rLv9wSCb7-1K3b_P_LzumvNM2pCH7A",
+    authDomain: "oshop-678a5.firebaseapp.com",
+    projectId: "oshop-678a5",
+    storageBucket: "oshop-678a5.appspot.com",
+    messagingSenderId: "162127482175",
+    appId: "1:162127482175:web:c2477046f737cdf54bd78a",
+    measurementId: "G-SRMXNF1T4R" 
+  }
 };
 
 /*
